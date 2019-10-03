ad910ecff41d7f8f610d8bd6ffdc1817f4f9eeb1
kimtimmy93 <kimtimmy93@gmail.com>
2019-10-03 13:16:56 -0700

    Commit 16 - Feed, Play, Lights button deactivated once pet dies

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+$('#hunger').hide();
+$('#boredom').hide();
+$('#sleepiness').hide();
+$('#level').hide();
+$('#gameTimer').hide();
+$('#name').hide();
+$('#rick1').hide();
+
 $('#start').on('click', () => {
     console.log('button works')
     game.setSleepiness();
@@ -6,30 +14,45 @@ $('#start').on('click', () => {
     game.setGameTimer();
     game.setLevel();
 
-
     const name = prompt("Enter your RickName!", "Your RickName here")
     const rick = new Tomagotchi(name);
     rick.setName();
 
+    $('#hunger').show();
+    $('#boredom').show();
+    $('#sleepiness').show();
+    $('#level').show();
+    $('#gameTimer').show();
+    $('#name').show();
+    $('#rick1').show();
   });
+
+//   $('#reset').on('click', () => {
+
+//   });
 
 $('#feed').on('click', () => {
     if(game.hunger > 0){
         game.hunger--
         $('#hunger').text(`Hunger: ${game.hunger}`)
-    }   
+    } 
 });
 $('#play').on('click', () => {
     if(game.boredom > 0){
         game.boredom--
         $('#boredom').text(`Boredom: ${game.boredom}`)
-    }
+    } 
 });
 $('#lights').on('click', () => {
     if(game.sleepiness > 0){
         game.sleepiness--
         $('#sleepiness').text(`Sleepiness: ${game.sleepiness}`)
-    }
+    } 
+});
+$('button').click(function() { 
+    $('#feed').off('click');
+    $('#play').off('click');
+    $('#lights').off('click');
 });
 
 
@@ -59,15 +82,29 @@ setGameTimer () {
         
         if(this.gameTimer === 0){
             clearInterval(interval); //stops the interval
+            $('#rick1').attr('src', 'go.gif')
+            $('#rick1').attr('id', 'gameover')
         } else if(this.hunger === 10) {
+            $('#rick1').attr('src', 'ricktomb.png')
+            $('#rick1').attr('id','deadrick')
+            $('#rick2').attr('src', 'ricktomb.png')
+            $('#rick2').attr('id','deadrick')
             $('#rick3').attr('src', 'ricktomb.png')
             $('#rick3').attr('id','deadrick')
             clearInterval(interval); 
         } else if(this.sleepiness === 10){
+            $('#rick1').attr('src', 'ricktomb.png')
+            $('#rick1').attr('id','deadrick')
+            $('#rick2').attr('src', 'ricktomb.png')
+            $('#rick2').attr('id','deadrick')
             $('#rick3').attr('src', 'ricktomb.png')
             $('#rick3').attr('id','deadrick')
             clearInterval(interval); 
         } else if(this.boredom === 10){
+            $('#rick1').attr('src', 'ricktomb.png')
+            $('#rick1').attr('id','deadrick')
+            $('#rick2').attr('src', 'ricktomb.png')
+            $('#rick2').attr('id','deadrick')
             $('#rick3').attr('src', 'ricktomb.png')
             $('#rick3').attr('id','deadrick')
             clearInterval(interval); 
@@ -75,7 +112,6 @@ setGameTimer () {
             this.gameTimer--
         }
         $gameTimer.text(`Timer: ${this.gameTimer}s`)
-
         }, 1000) 
     },
     setHunger() {
@@ -90,13 +126,14 @@ setGameTimer () {
                 clearInterval(int);
              } else if(this.sleepiness === 10){
                 clearInterval(int);
+             } else if(this.gameTimer === 0) {
+                clearInterval(int);
              }
-        },3000)   
+        },Math.floor(Math.random()*(5000 - 1000 + 1) + 1000))   
     },
     setSleepiness() {
         const $sleepiness = $('#sleepiness')
-    
-        const int = setInterval(() => {
+        const int = setInterval(() => { 
          this.sleepiness++
          $sleepiness.text(`Sleepiness: ${this.sleepiness}`)
              if(this.sleepiness === 10){
@@ -105,13 +142,13 @@ setGameTimer () {
                 clearInterval(int);
               } else if(this.boredom === 10) {
                 clearInterval(int);
-              } 
-         },1500) 
-                 
+              } else if(this.gameTimer === 0) {
+                clearInterval(int);
+             }
+         },Math.floor(Math.random()*(5000 - 1000 + 1) + 1000));          
     },
     setBoredom() {
         const $boredom = $('#boredom')
-    
        const int = setInterval(() => {
         this.boredom++
         $boredom.text(`Boredom: ${this.boredom}`)
@@ -121,8 +158,10 @@ setGameTimer () {
                 clearInterval(int);
              } else if(this.sleepiness === 10){
                 clearInterval(int);
+             } else if(this.gameTimer === 0) {
+                clearInterval(int);
              }
-        },2000)         
+        },Math.floor(Math.random()*(5000 - 1000 + 1) + 1000))         
     },
     setLevel (){
         const $level = $('#level')
@@ -142,21 +181,27 @@ setGameTimer () {
                     alert(`Your Rick has died :(`)
                  } else if(this.gameTimer === 0){
                      clearInterval(int);
-                 } else if(this.level < 2){
+                 } else if(this.level < 5){
                     $('#rick1').attr('src', 'https://i.redd.it/93d8smnl64qx.gif')
-                 } else if(this.level === 2){
+                 } else if(this.level === 5){
                     $('#rick1').attr('src', 'https://media.giphy.com/media/5wFHChzgBtfANDa76U/giphy.gif')
                     $('#rick1').attr('id', 'rick2')
-                 } else if(this.level === 3){
+                 } else if(this.level === 10){
                     $('#rick2').attr('src', 'picklerick.gif')
                     $('#rick2').attr('id', 'rick3')
+                 } else if(this.gameTimer === 0) {
+                    clearInterval(int);
                  }
-            },2000)
-
+            },3000)
     }
-       
 }
 console.log(game)
+
+
+// deactivate buttons once pet dies
+// once pet dies, start over after alert
+// deactivate everything when timer hits 0
+
 
 
 
